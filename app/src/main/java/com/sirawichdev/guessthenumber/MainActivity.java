bfedd3fc88aec408a606a48a6ddc5a8bba5c16ba
SirawichDev <sirawit0676@gmail.com>
2018-03-02 01:02:33 +0700

    Fnished yeah

--- app/src/main/java/com/sirawichdev/guessthenumber/MainActivity.java
+++ app/src/main/java/com/sirawichdev/guessthenumber/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sirawichdev.guessthenumber;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -9,8 +10,10 @@ import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +25,7 @@ public class MainActivity extends Activity {
     Locale myLocale;
     int randomNumber;
     int guesses;
+    Spinner spinnerctrl;
     public TextView title;
     public TextView guessCounter;
     String titleText;
@@ -36,12 +40,12 @@ public class MainActivity extends Activity {
 
         String toastMessage = "";
         title = (TextView) findViewById(R.id.title);
-
+        Context c = MainActivity.this;
         //This is where the magic happens
         if (guessedNumberInt == randomNumber && guesses <= 5) {
             guesses++;
-            toastMessage = "You're right! I was thinking of " + randomNumber + ". It took you " + guesses + " guesses!";
-            titleText = "New Game. Enter Your Guess Below!";
+            toastMessage = c.getResources().getString(R.string.right) + randomNumber + c.getResources().getString(R.string.its) + guesses +c.getResources().getString(R.string.time) ;
+            titleText = c.getResources().getString(R.string.news);
 
             Random randomGenerator = new Random();
             randomNumber = randomGenerator.nextInt(50);
@@ -49,36 +53,37 @@ public class MainActivity extends Activity {
 
         }
         else if (guessedNumberInt == randomNumber+3 || guessedNumberInt == randomNumber-3 && guesses <= 5 ) {
-            toastMessage = "Nearest!. Guess More";
-            titleText = guessedNumberInt + " Nearest!.";
+            toastMessage = c.getResources().getString(R.string.near);
+            titleText = guessedNumberInt +" "+  c.getResources().getString(R.string.nearest);
             guesses++;
         }
         else if (guessedNumberInt > randomNumber && guesses <= 5) {
-            toastMessage = "Too High. Guess Lower.";
-            titleText = guessedNumberInt + " is too High. Guess Lower.";
+            toastMessage =c.getResources().getString(R.string.high) ;
+            titleText = guessedNumberInt +" "+  c.getResources().getString(R.string.high);
             guesses++;
         }
 
         else if (guessedNumberInt < randomNumber && guesses <= 5) {
-            toastMessage = "Too Low. Guess Higher.";
-            titleText = guessedNumberInt + " is too Low. Guess Higher.";
+            toastMessage = c.getResources().getString(R.string.low) ;
+            titleText = guessedNumberInt +" "+ c.getResources().getString(R.string.low) ;
             guesses++;
 
         }
 
         if(guesses > 5) {
-            toastMessage = "You're guesses More than 5 Time It's Over :3 ";
-            titleText = "It's Game Over Les't try New Game.";
+            toastMessage = c.getResources().getString(R.string.more);
+            titleText = c.getResources().getString(R.string.trys);
 
             Random randomGenerator = new Random();
             randomNumber = randomGenerator.nextInt(50);
             guesses = 0;
         }
+
         Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_LONG).show();
         title.setText(titleText);
         guessedNumber.setText("");
         guessCounter = (TextView) findViewById(R.id.guessCounter);
-        guessCounter.setText("Guesses: " + guesses);
+        guessCounter.setText(c.getResources().getString(R.string.guess) + guesses);
 
     }
 
@@ -98,32 +103,36 @@ public class MainActivity extends Activity {
                 guessCommence(v);
             }
         });
+        spinnerctrl = (Spinner) findViewById(R.id.spinner1);
+        spinnerctrl.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+
+                if (pos == 1) {
+
+                    Toast.makeText(parent.getContext(),
+                            "You have selected thai", Toast.LENGTH_SHORT)
+                            .show();
+                    setLocale("th");
+                } else if (pos == 2) {
+
+                    Toast.makeText(parent.getContext(),
+                            "You have selected english", Toast.LENGTH_SHORT)
+                            .show();
+                    setLocale("en");
+                }
+
+            }
+
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+
+        });
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            setLocale("en");
-            return true;
-        }
-
-
-        return super.onOptionsItemSelected(item);
-    }
     public void setLocale(String lang) {
 
         myLocale = new Locale(lang);
@@ -136,4 +145,13 @@ public class MainActivity extends Activity {
         startActivity(refresh);
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
 }
